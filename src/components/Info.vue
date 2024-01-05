e6efc0e46fc9c37f1a52acd6d0ff095ab8e747bb
drinkingtheink <jason.m.harrison@gmail.com>
2024-01-04 22:39:20 -0600

    about links

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="info-links">
+      <div class="icon">
+        <span class="chevron"></span>
+      </div>
       <div v-for="link in this.info" :key="link.link">
           <a 
             :href="link.link"
@@ -20,3 +23,46 @@ export default {
   },
 }
 </script>
+
+<style> 
+.chevron::before {
+	border-style: solid;
+	border-width: 0.25em 0.25em 0 0;
+	content: '';
+	display: inline-block;
+	height: 0.45em;
+	left: 0.15em;
+	position: relative;
+	top: 0.15em;
+	transform: rotate(-45deg);
+	vertical-align: top;
+	width: 0.45em;
+}
+
+.chevron.right:before {
+	left: 0;
+	/* transform: rotate(45deg); */
+}
+
+.icon {
+  --iconDim: 70px;
+
+  height: var(--iconDim);
+  width: var(--iconDim);
+  border-radius: 50%;
+  background-color: var(--overlay);
+  position: absolute;
+  left: -3rem;
+  top: 25px;
+  box-shadow: 0px 0px 10px 5px rgba(0,0,0,0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transform: rotate(90deg);
+  font-size: 160%;
+}
+
+.icon span {
+  color: var(--base, #222);
+}
+</style>
